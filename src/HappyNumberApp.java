b8dceec2c574893e797cd751a462d2c79251c906
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-01-13 13:40:23 +0330

    Add method get-number-of-usre in happy-number-app class

--- src/HappyNumberApp.java
+++ src/HappyNumberApp.java
@@ -1,3 +1,21 @@
-public class HappyNumber {
-    static
+import java.util.Scanner;
+
+public class HappyNumberApp {
+
+
+    public static Long getNumberOfUser(){
+        Scanner scanner = new Scanner(System.in);
+        long num;
+
+        do {
+            System.out.println("Enter a positive number: ");
+            while (!scanner.hasNextLong()) {
+                System.out.println("Invalid input! Please enter a valid positive number: ");
+                scanner.next();
+            }
+            num = scanner.nextLong();
+        } while (num <= 0);
+        return num;
+    }
+
 }
