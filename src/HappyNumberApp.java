1fba07f404f04335b9b1922e620be0970cdc2cb6
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-01-13 13:45:03 +0330

    Update happy-number-app class and implementaion LongFunction for find happy number.

--- src/HappyNumberApp.java
+++ src/HappyNumberApp.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
+import java.util.function.LongFunction;
 
 public class HappyNumberApp {
+
+    public static void main(String[] args) {
+
+        long number = getNumberOfUser();
+
+        LongFunction<Boolean> happyNumber = n -> {
+            while (n > 9) {
+                long sum = 0;
+                while (n > 0) {
+                    long digit = n % 10;
+                    sum += digit * digit;
+                    n /= 10;
+                }
+                n = sum;
+            }
+            return n == 1;
+        };
+        System.out.println(happyNumber.apply(number));
+
+    }
 
 
     public static Long getNumberOfUser(){
